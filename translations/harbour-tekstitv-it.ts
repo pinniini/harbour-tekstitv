aa35f94630a3fd8bf129ce288addd8a3925f6fd4
Francesco Gazzetta <fgaz@fgaz.me>
2026-01-28 18:05:50 +0100

    Add English and Italian translations

---
+++ translations/harbour-tekstitv-it.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Löysitkö virheen? Onko sinulla hyviä kehitysideoita? Raportoi ne github:iin ja minä katson ne mielelläni läpi :)</source>
+        <translation>Hai riscontrato un problema? Hai un&apos;idea per migliorare questa app? Segnalali su GitHub e sarò felice di darci un&apos;occhiata :)</translation>
+    </message>
+    <message>
+        <source>Tietoja</source>
+        <translation>Info su</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Tekijä</source>
+        <translation>Autore</translation>
+    </message>
+    <message>
+        <source>Lähdekoodin lisenssi</source>
+        <translation>Licenza del codice sorgente</translation>
+    </message>
+    <message>
+        <source>Lähdekoodi</source>
+        <translation>Codice sorgente</translation>
+    </message>
+    <message>
+        <source>Ideoita/Ongelmia/Haluatko auttaa</source>
+        <translation>Idee/problemi/vuoi aiutare?</translation>
+    </message>
+    <message>
+        <source>pinniini (Joni Korhonen)</source>
+        <translation>pinniini (Joni Korhonen)</translation>
+    </message>
+    <message>
+        <source>Tämä on helppokäyttöinen teksti-tv sovellus. Tällä hetkellä sovellus tukee YLE:n ja MTV:n teksti-tv:tä. Muita lähteitä saatan lisätä tulevaisuudessa (mikäli tiedot on helposti saatavilla). Sovellus on avointa lähdekoodia ja koodit löytyvät github:sta.</source>
+        <translation>Questa è un&apos;applicazione per il teletext semplice da usare. Al momento, l&apos;app supporta i teletext YLE e MTV finlandesi. Altre sorgenti potrebbero essere aggiunte in futuro (se le informazioni sono facilmente reperibili). L&apos;applicazione è open source e il codice può essere trovato su GitHub.</translation>
+    </message>
+    <message>
+        <source>Versio </source>
+        <translation>Versione </translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Tietoja</source>
+        <translation>Info su</translation>
+    </message>
+    <message>
+        <source>Lisää suosikki</source>
+        <translation>Aggiungi ai preferiti</translation>
+    </message>
+    <message>
+        <source>Lataa uudelleen</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <source>Aseta lähteen aloitussivuksi</source>
+        <translation>Imposta questa sorgente come predefinita</translation>
+    </message>
+    <message>
+        <source>Pysäytä automaattinen päivitys</source>
+        <translation>Disabilita l&apos;aggiornamento automatico</translation>
+    </message>
+    <message>
+        <source>Päivitä automaattisesti</source>
+        <translation>Abilita l&apos;aggiornamento automatico</translation>
+    </message>
+    <message>
+        <source>Asetukset</source>
+        <translation>Impostazioni</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Asetukset</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Lähde</source>
+        <translation>Sorgente</translation>
+    </message>
+    <message>
+        <source>Oletuslähde</source>
+        <translation>Sorgente predefinita</translation>
+    </message>
+    <message>
+        <source>Ei mikään</source>
+        <translation>Niente</translation>
+    </message>
+    <message>
+        <source>Lähde, jonka aloitussivu ladataan automaattisesti kun sovellus avataan.</source>
+        <translation>La sorgente che viene caricata automaticamente all&apos;avvio dell&apos;app.</translation>
+    </message>
+    <message>
+        <source>Sivunkatselin</source>
+        <translation>Vista zoomata</translation>
+    </message>
+    <message>
+        <source>Oletustarkennus</source>
+        <translation>Livello di zoom predefinito</translation>
+    </message>
+    <message>
+        <source>Oletustarkennuksen taso, jolla sivu näytetään oletuksena. Sivua voi myös tarkentaa nipistys-eleellä. Sivunkatselin avataan napauttamalla teksti-tv:n sivun kuvaa.</source>
+        <translation>Il livello di zoom predefinito al quale la pagina è visualizzata nella vista zoomata. Puoi anche zoomare allargando/avvicinando le dita. Per aprire la vista zoomata, tocca l&apos;immagine della pagina teletext.</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesPage</name>
+    <message>
+        <source>Lähteet</source>
+        <translation>Sorgenti</translation>
+    </message>
+    <message>
+        <source>Aseta oletukseksi</source>
+        <translation>Imposta come predefinita</translation>
+    </message>
+    <message>
+        <source>Poista oletus</source>
+        <translation>Rimuovi predefinita</translation>
+    </message>
+    <message>
+        <source>Tietoja</source>
+        <translation>Info su</translation>
+    </message>
+</context>
+</TS>
